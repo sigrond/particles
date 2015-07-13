fb7e146c073e40a8b5714d5776f7f4186a69e320
sigrond <sigrond93@gmail.com>
2015-07-13 14:08:44 +0200

    reczna naprawa

--- particles_kernel.cuh
+++ particles_kernel.cuh
@@ -62,52 +62,6 @@ struct SimParams
 
 	int particleTypesNum;
 	unsigned long long int brownQuality;
-<<<<<<< HEAD
-};
-
-#endif
-
-#ifndef PARTICLES_KERNEL_H
-#define PARTICLES_KERNEL_H
-
-#define USE_TEX 0
-
-#if USE_TEX
-#define FETCH(t, i) tex1Dfetch(t##Tex, i)
-#else
-#define FETCH(t, i) t[i]
-#endif
-
-#include "vector_types.h"
-typedef unsigned int uint;
-
-// simulation parameters
-struct SimParams
-{
-    float3 colliderPos;
-    float  colliderRadius;
-
-    float3 gravity;
-    float globalDamping;
-    float particleRadius;
-
-    uint3 gridSize;
-    uint numCells;
-    float3 worldOrigin;
-    float3 cellSize;
-
-    uint numBodies;
-    uint maxParticlesPerCell;
-
-    float spring;
-    float damping;
-    float shear;
-    float attraction;
-    float boundaryDamping;
-
-	float bigradius;
-=======
->>>>>>> sigrond
 };
 
 #endif
