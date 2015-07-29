f7bd92b8d3f7d50875f57b4c2c82cfeb42374582
sigrond <sigrond93@gmail.com>
2015-07-29 16:36:09 +0200

    :gem: drobne korekty i testowanie

--- particles.cpp
+++ particles.cpp
@@ -979,7 +979,7 @@ main(int argc, char **argv)
 #ifdef _DEBUG
     std::cout<<"Debug Build!\n";
 #endif // _DEBUG
-	std::cout<<"Uwaga różne typy cząstek!\nKonfiguracja w particleType.cfg\nWyświetlany rozmiar cząstek na razie nie odpowiada wartościom symulacji!\n";
+	std::cout<<"Uwaga różne typy cząstek!\nKonfiguracja w particleType.cfg\n";
 	std::string plikKonfiguracyjny("particleType.cfg");
     particleTypesLoader pTLoader(plikKonfiguracyjny);
     pTLoader.loadTypes(typyCzastek);
